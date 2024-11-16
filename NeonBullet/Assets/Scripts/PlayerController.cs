624b61125799081b572adde7d10c68ad17f5cfee
Arthur Pradier <arthur.pradier81@gmail.com>
2024-11-16 15:31:20 +0100

    feat(gun): Gun can shoot enemies, and hurt player
    Title

--- NeonBullet/Assets/Scripts/PlayerController.cs
+++ NeonBullet/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,12 @@
+using System;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public int lives = 3;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +17,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Hit()
+    {
+        lives--;
+        // if(lives <= 0)
+        // {
+        //     GameManager.Instance.GameOver();
+        // }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.CompareTag("Enemy"))
+            Hit();
     }
 }
