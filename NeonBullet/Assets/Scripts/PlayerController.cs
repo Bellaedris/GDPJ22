f25c30236496f1138e54b01f9bdc3c7d6468a19c
Enzo Martinez <killerbackofheart@gmail.com>
2024-11-16 19:04:49 +0100

    Ajout degat joueur hud

--- NeonBullet/Assets/Scripts/PlayerController.cs
+++ NeonBullet/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,16 @@
 using System;
+using System.Collections;
 using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     public int lives = 3;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject HUD;
+    public Sprite[] damagedHUD;
+    private bool isInvulnerable = false;
 
     public void Hit()
     {
@@ -28,9 +21,36 @@ public class PlayerController : MonoBehaviour
         // }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.collider.CompareTag("Enemy"))
+        if (other.CompareTag("Enemy") && !isInvulnerable)
+        {
+            StartCoroutine(IsEnumeratorSwitchState());
             Hit();
+            DisplayDamageUI();
+        }
+    }
+
+    private void DisplayDamageUI()
+    {
+        if (lives == 2)
+        {
+            HUD.GetComponent<Image>().sprite = damagedHUD[0];
+        }
+        else if (lives == 1)
+        {
+            HUD.GetComponent<Image>().sprite = damagedHUD[1];
+        }
+        else if (lives == 0)
+        {
+            HUD.GetComponent<Image>().sprite = damagedHUD[2];
+        }
+    }
+
+    private IEnumerator IsEnumeratorSwitchState()
+    {
+        isInvulnerable = true;
+        yield return new WaitForSeconds(5);
+        isInvulnerable = false;
     }
 }
