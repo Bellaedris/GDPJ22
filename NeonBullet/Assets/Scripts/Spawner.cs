2a34e8c1bfd8c32a5f709fe6d933b23784c7c0b0
Enzo Martinez <killerbackofheart@gmail.com>
2024-11-17 12:25:21 +0100

    Ajout spawner enemy + reglage pb deplacement joueurs

---
+++ NeonBullet/Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] doors;
+    public GameObject[] enemies;
+    public AudioManager audioManager;
+    public GameObject player;
+
+    public void Start()
+    {
+        InvokeRepeating("spawn", 30f, 15f);
+    }
+
+    void spawn()
+    {
+        int randomDoor = Random.Range(0, doors.Length);
+        int randomEnemies = Random.Range(0, enemies.Length);
+
+        GameObject gm = Instantiate(enemies[randomEnemies]);
+        gm.transform.position = doors[randomDoor].transform.position;
+        gm.GetComponent<EnemyAI>().player = player;
+        gm.GetComponent<EnemyAI>()._audioManager = audioManager;
+
+    }
+}
